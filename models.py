e0e960547473b59b5fff07980e13b966abbb160b
Akis Linardos <linardos.akis@gmail.com>
2025-04-30 23:01:15 -0400

    phew

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class ResNet9(nn.Module):
     def __init__(self):
